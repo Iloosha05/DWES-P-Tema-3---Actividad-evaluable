3f48623847367f57f1529b66bef3ded735504660
Iloosha05 <zilber.ilia@gmail.com>
2025-11-24 09:24:02 +0100

    Enemy and item models added

--- model/Enemy.php
+++ model/Enemy.php
@@ -1,4 +1,161 @@
 <?php
 
 class Enemy {
+    private $id;
+    private $name;
+    private $description;
+    private $isBoss;
+    private $health;
+    private $strength;
+    private $defense;
+    private $img;
+
+    private $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function getId() { 
+        return $this->id; 
+    }
+
+    public function setId($id) { 
+        $this->id = $id; 
+        return $this; 
+    }
+
+    public function getName() { 
+        return $this->name; 
+    }
+
+    public function setName($name) { 
+        $this->name = $name; 
+        return $this; 
+    }
+
+    public function getDescription() { 
+        return $this->description; 
+    }
+
+    public function setDescription($description) { 
+        $this->description = $description; 
+        return $this; 
+    }
+
+    public function getIsBoss() { 
+        return $this->isBoss; 
+    }
+
+    public function setIsBoss($isBoss) { 
+        $this->isBoss = $isBoss; 
+        return $this; 
+    }
+
+    public function getHealth() { 
+        return $this->health; 
+    }
+
+    public function setHealth($health) { 
+        $this->health = $health; 
+        return $this; 
+    }
+
+    public function getStrength() { 
+        return $this->strength; 
+    }
+
+    public function setStrength($strength) { 
+        $this->strength = $strength; 
+        return $this; 
+    }
+
+    public function getDefense() { 
+        return $this->defense; 
+    }
+
+    public function setDefense($defense) { 
+        $this->defense = $defense; 
+        return $this; 
+    }
+
+    public function getImg() { 
+        return $this->img; 
+    }
+
+    public function setImg($img) { 
+        $this->img = $img; 
+        return $this; 
+    }
+
+    public function save()
+    {
+        if ($this->id) {
+            $stmt = $this->db->prepare(
+                "UPDATE enemies
+                    SET name = :name,
+                     description = :description,
+                    isBoss = :isBoss,
+                    health = :health,
+                    strength = :strength,
+                    defense = :defense,
+                    img = :img
+                    WHERE id = :id"
+            );
+            $stmt->bindParam(':id', $this->id);
+        } else {
+            $stmt = $this->db->prepare(
+                "INSERT INTO enemies 
+                    (name, description, isBoss, health, strength, defense, img)
+                 VALUES (:name, :description, :isBoss, :health, :strength, :defense, :img)"
+            );
+        }
+
+        $isBossVal = $this->getIsBoss() ? 1 : 0;
+
+        $stmt->bindParam(':name', $this->getName());
+        $stmt->bindParam(':description', $this->getDescription());
+        $stmt->bindParam(':isBoss', $isBossVal);
+        $stmt->bindParam(':health', $this->getHealth());
+        $stmt->bindParam(':strength', $this->getStrength());
+        $stmt->bindParam(':defense', $this->getDefense());
+        $stmt->bindParam(':img', $this->getImg());
+        return $stmt->execute();
+    }
+
+    public function loadById($id)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM enemies WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($data) {
+            $this->id = $data['id'];
+            $this->name = $data['name'];
+            $this->description = $data['description'];
+            $this->isBoss = $data['isBoss'];
+            $this->health = $data['health'];
+            $this->strength = $data['strength'];
+            $this->defense = $data['defense'];
+            $this->img = $data['img'];
+            return true;
+        }
+        return false;
+    }
+
+    public function getAll()
+    {
+        $stmt = $this->db->prepare("SELECT * FROM enemies");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+    public function delete($id)
+    {
+        $stmt = $this->db->prepare("DELETE FROM enemies WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        return $stmt->execute();
+    }
 }
--- model/item.php
+++ model/item.php
@@ -1,4 +1,132 @@
 <?php
-
+    
 class Item {
+    private $id;
+    private $name;
+    private $description;
+    private $type;
+    private $effect;
+    private $img;
+
+    private $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function getId() { 
+        return $this->id; 
+    }
+
+    public function setId($id) { 
+        $this->id = $id; 
+        return $this; 
+    }
+
+    public function getName() { 
+        return $this->name; 
+    }
+
+    public function setName($name) { 
+        $this->name = $name; 
+        return $this; 
+    }
+
+    public function getDescription() { 
+        return $this->description; 
+    }
+
+    public function setDescription($description) { 
+        $this->description = $description; 
+        return $this; 
+    }
+
+    public function getType() { 
+        return $this->type; 
+    }
+
+    public function setType($type) { 
+        $this->type = $type; 
+        return $this; 
+    }
+
+    public function getEffect() { 
+        return $this->effect; 
+    }
+
+    public function setEffect($effect) { 
+        $this->effect = $effect; 
+        return $this; 
+    }
+
+    public function getImg() { 
+        return $this->img; 
+    }
+
+    public function setImg($img) { 
+        $this->img = $img; 
+        return $this; 
+    }
+
+    public function save()
+    {
+        if ($this->id) {
+            $stmt = $this->db->prepare(
+                "UPDATE items
+                    SET name = :name,
+                    description = :description,
+                    type = :type,
+                    effect = :effect,
+                    img = :img
+                    WHERE id = :id"
+            );
+            $stmt->bindParam(':id', $this->id);
+        } else {
+            $stmt = $this->db->prepare(
+                "INSERT INTO items 
+                    (name, description, type, effect, img)
+                 VALUES (:name, :description, :type, :effect, :img)"
+            );
+        }
+        $stmt->bindParam(':name', $this->getName());
+        $stmt->bindParam(':description', $this->getDescription());
+        $stmt->bindParam(':type', $this->getType());
+        $stmt->bindParam(':effect', $this->getEffect());
+        $stmt->bindParam(':img', $this->getImg());
+        return $stmt->execute();
+    }
+
+    public function loadById($id)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM items WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($data) {
+            $this->id = $data['id'];
+            $this->name = $data['name'];
+            $this->description = $data['description'];
+            $this->type = $data['type'];
+            $this->effect = $data['effect'];
+            $this->img = $data['img'];
+            return true;
+        }
+        return false;
+    }
+
+    public function getAll()
+    {
+        $stmt = $this->db->prepare("SELECT * FROM items");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function delete($id)
+    {
+        $stmt = $this->db->prepare("DELETE FROM items WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        return $stmt->execute();
+    }
 }
